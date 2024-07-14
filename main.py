23167a6fd638aa3ec5832137cdfad1c16bd5c359
Lordturkola <andreasdunca@yahoo.se>
2024-07-14 19:31:22 +0200

    why did i waste so muc time on this, replace with ps1 setup instead

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import sys, os
 from webapp.app import WebApp
 from mediaEventManager import MediaEventManager
-import shutil, sys, os
-
 current = os.path.abspath(os.curdir)
 env = os.path.join(current, "environment", "soundboard")
 
@@ -11,11 +10,11 @@ reqs_path = os.path.join(current, "environment", "requirements.txt")
 winVenvPath = os.path.join(env, "Scripts", "Activate.bat")
 linuxVenvPath = os.path.join(env, "bin", "activate")
 shell_command = {
-    "src_win32": f"{winVenvPath}",
+    "src_win32": f"CALL {winVenvPath}",
     "cp_win32": f"copy {ffmpeg_env} {dst}",
     "cp_linux": f"cp {ffmpeg_env} {dst}",
     "src_linux": f"source {linuxVenvPath}",
-    "venv": "python -m venv soundboard",
+    "venv": f"python3 -m venv {env}",
     "pip": f"pip install -r {reqs_path}",
 }
 if __name__ == "__main__":
@@ -26,23 +25,9 @@ if __name__ == "__main__":
     print(winVenvPath)
     print(linuxVenvPath)
     print(reqs_path)
-    if not os.path.exists(env):
-        print("creating new venv")
-        os.system(shell_command["venv"])
-    else:
-        print("venv already exists")
-
     print("installing reqs")
-    os.system(shell_command["pip"])
-    if sys.platform == "win32" or sys.platform == "cygwin":
-        print(f"sourcing {sys.platform} {shell_command['src_win32']}")
-        os.system(shell_command["src_win32"])
-        print("copying ffmpeg dependency to correct folder")
-        os.system(shell_command["cp_win32"])
-    else:
-        print(f"sourcing {sys.platform} {shell_command['src_linux']}")
-        os.system(shell_command["src_linux"])
-        os.system(shell_command["cp_win32"])
+    #os.system(shell_command["pip"])
+#  
 
     # MediaEventManager.start_media_manager()
     # WebApp.start_server(True)
