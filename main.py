a52d9cc8a51a4eb03ff573e8c57cef7dda05c676
Lordturkola <andreasdunca@yahoo.se>
2024-07-15 23:16:17 +0200

    changed setup for ffmepg dependency

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ current = os.path.abspath(os.curdir)
 env = os.path.join(current, "environment", "soundboard")
 
 ffmpeg_env = os.path.join(current, "ffmpeg", "bin", "*")
-dstWin = os.path.join(env, "Lib", "site-packages", "yt_dlp")
+dstWin = os.path.join(env, "Lib", "site-packages", "yt_dlp", "ffmpeg")
 python_ver_num = sys.version.split()[0].split(".")
 python_ver = f"python{python_ver_num[0]}.{python_ver_num[1]}"
 dstLinux = os.path.join(env, "lib", python_ver, "site-packages", "yt_dlp", "ffmpeg")
@@ -15,6 +15,8 @@ shell_command = {
     "src_win32": f"CALL {winVenvPath}",
     "cp_win32": f"copy {ffmpeg_env} {dstWin}",
     "cp_linux": f"cp {ffmpeg_env} {dstLinux}",
+    "mkdir_win": f"mkdir {dstWin}",
+    "mkdir_linux": f"mkdir {dstLinux}",
     "src_linux": f"source {linuxVenvPath}",
     "venv": f"python3 -m venv {env}",
     "pip": f"pip install -r {reqs_path}",
@@ -30,11 +32,13 @@ if __name__ == "__main__":
     print(reqs_path)
     print(f"OS system: {sys.platform}")
     print("installing dependencies")
+    
     os.system(shell_command["pip"])
     print("additional dependencies")
     if sys.platform == "win32" or sys.platform == "cygwin":
         if not os.path.exists(dstWin):
             print("copying ffmpeg bin to venv (third party dependency)")
+            os.system(shell_command["mkdir_win"])
             os.system(shell_command["cp_win32"])
         else:
             print("already exists")
@@ -42,6 +46,7 @@ if __name__ == "__main__":
     else:
         if not os.path.exists(dstLinux):
             print("copying ffmpeg bin to venv (third party dependency)")
+            os.system(shell_command["mkdir_linux"])
             os.system(shell_command["cp_linux"])
         else:
             print("already exists")
@@ -50,9 +55,11 @@ if __name__ == "__main__":
     from mediaEventManager import MediaEventManager
     from threading import Thread
 
-    t2 = Thread(target=WebApp.start_server, args=())
-    t2.start()
-    t1 = Thread(target=MediaEventManager.start_media_manager, args=())
+    t1 = Thread(target=WebApp.start_server, args=())
     t1.start()
+    t2 = Thread(target=MediaEventManager.start_media_manager, args=())
+    t2.start()
+    
     t1.join()
     t2.join()
+
